97eb5a59bfccf2f13374b9898eb6a2dfb30bdd1e
pepece <ppcordier@gmail.com>
2017-05-23 17:17:48 +0200

    Correction de choix groupe

--- WebContent/View/js/choixGroupe.js
+++ WebContent/View/js/choixGroupe.js
@@ -4,26 +4,45 @@
  */
 $(document).ready(function() {
 	$("[data-toggleStudent-element]").slideToggle();
-	$('[data-toggle-element-promid]').hide(); 
-	$('[data-toggle-element-groupeid]').hide(); 
+	$('[data-toggle-element-promid]').hide();
+	$('[data-toggle-element2-promid]').hide(); 
+	
  });
 
 /**
  * function to toggle student
  */
 $("[data-toggleStudent]").click(function(){
-//	console.log($(this).data('groupeid'));
+	console.log($(this).data('groupeid'));
 	$(this).parent().parent().children("[data-toggleStudent-element]").slideToggle();
 	$(this).toggleClass('glyphicon-triangle-bottom glyphicon-menu-right');
 }); 
 
+//function hide(groupe)
+//{
+//$	normalement il reste à repasser en unselectedc
+//}
+
+
+function toggleGroup(div, prom)
+{
+	div.each(function(){
+		if($(this).data('toggle-element2-promid')==prom)
+		{
+			$(this).slideToggle();
+		}
+	});
+}
+
 /**
  * function to toggle group
  */
-$("[data-groupeid]").click(function(){    
-	$('[data-toggle-element-groupeid="'+$(this).data('groupeid')+'"]').slideToggle();    
+$("[data-groupeid]").click(function(){   
+//	for each
+	toggleGroup($('[data-toggle-element-groupeid="'+$(this).data('groupeid')+'"]'), $(this).parent().parent().data('toggle-element-promid'));
 	$(this).toggleClass('selected unselected');
 }); 
+
 
 /**
  * function to toggle prom
@@ -31,5 +50,6 @@ $("[data-groupeid]").click(function(){
 $("[data-promid]").click(function(){    
 	$(this).toggleClass('selected unselected');
 	$('[data-toggle-element-promid="'+$(this).data('promid')+'"]').slideToggle(); 
-	$('[data-toggle-element2-promid="'+$(this).data('promid')+'"]').hide(); 
+	$('[data-toggle-element-promid="'+$(this).data('promid')+'"]').children().find(".selected").toggleClass('selected unselected');
+	$('[data-toggle-element2-promid="'+$(this).data('promid')+'"]').hide();
 }); 
