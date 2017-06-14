2accea9e0a530e189d271db1019cc7e4b5067dd0
LaetitiaBouvier <laetitia.bouvier@isep.fr>
2017-06-14 21:12:30 +0200

    premieres modifs pour promo, groupe, sous groupe

--- src/gestiondesgrillesapp/controller/CreationEleveServlet.java
+++ src/gestiondesgrillesapp/controller/CreationEleveServlet.java
@@ -14,16 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 import gestiondesgrillesapp.model.*;
 
 /**
- * Servlet implementation class CreationEleve
+ * Servlet implementation class CreationEleveServlet
  */
-@WebServlet("/CreationEleve")
-public class CreationEleve extends HttpServlet {
+@WebServlet("/CreationEleveServlet")
+public class CreationEleveServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreationEleve() {
+    public CreationEleveServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -46,8 +46,7 @@ public class CreationEleve extends HttpServlet {
 			long nbColonnes = Long.parseLong(colonnes);
 			long i ;
 			long j ;
-			
-			//ATTENTION : l'ideal serait de preciser 
+			 
 			for (i=1; i<nbLignes+1; i++){
 				String numeroUser = request.getParameter("champ["+i+"][0]");
 				if (numeroUser != null){
@@ -55,10 +54,24 @@ public class CreationEleve extends HttpServlet {
 				String nomUser = request.getParameter("champ["+i+"][1]");
 				String prenomUser = request.getParameter("champ["+i+"][2]");
 				String mailUser = request.getParameter("champ["+i+"][3]");
-				String promotionUser = request.getParameter("champ["+i+"][4]");
-				String groupeUser = request.getParameter("champ["+i+"][5]");
-				String sousGroupeUser = request.getParameter("champ["+i+"][6]");
+				String nomPromotionUser = request.getParameter("champ["+i+"][4]");
+				String nomGroupeUser = request.getParameter("champ["+i+"][5]");
+				String nomSousGroupeUser = request.getParameter("champ["+i+"][6]");
 				User eleve = new User(nomUser, prenomUser, numeroUser, mailUser, false);
+				List<SousGroupe> sousGroupeList = em.createQuery("SELECT c FROM SousGroupe c WHERE nom="+sousGroupeUser, SousGroupe.class).getResultList();
+	        	if(sousGroupeList != null){
+	        		for (SousGroupe sg : sousGroupeList) {
+	        		long idSousGroupeComp = sg.getID();
+	        		long idGroupe = sg.getGroupeID();
+	        		}
+	        	if(sousGroupeList == null){
+	        		SousGroupe ssgrp = new SousGroupe(nomSousGroupeUser);
+	        		em.getTransaction().begin();	//
+					em.persist(eleve);					//
+					em.getTransaction().commit();	// Attention !!! les id's ne sont générées qu'après le commit de l'instance persistante associée !
+					ssgrp.setGroupeID(Long.parseLong(nomGroupeUser))
+	        		
+	        	}
 				em.getTransaction().begin();	//
 				em.persist(eleve);					//
 				em.getTransaction().commit();	// Attention !!! les id's ne sont générées qu'après le commit de l'instance persistante associée !
