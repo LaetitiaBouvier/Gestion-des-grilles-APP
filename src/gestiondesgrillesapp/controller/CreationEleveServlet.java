23dba97d14350ead4b5e23c7f80a210121d2ad53
Stéphane <Stphane@DESKTOP-65NBS10>
2017-06-15 15:05:47 +0200

    MAJ CreationEleveServlet

--- src/gestiondesgrillesapp/controller/CreationEleveServlet.java
+++ src/gestiondesgrillesapp/controller/CreationEleveServlet.java
@@ -32,6 +32,7 @@ public class CreationEleveServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
 		// Obtain a database connection:
 		EntityManagerFactory emf = (EntityManagerFactory)getServletContext().getAttribute("emf");
 		EntityManager em = emf.createEntityManager();
@@ -181,12 +182,162 @@ public class CreationEleveServlet extends HttpServlet {
 		}
 	}
 		
-		/**
-		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-			doGet(request, response);
-		}
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	// Obtain a database connection:
+    	EntityManagerFactory emf = (EntityManagerFactory)getServletContext().getAttribute("emf");
+    	EntityManager em = emf.createEntityManager();
+
+    	try {
+
+    		// nombres de lignes et de colonnes
+    		String lignes = request.getParameter("ligne");
+    		long nbLignes = Long.parseLong(lignes);
+
+    		for (int i = 1; i < nbLignes+1; i++)
+    		{
+    			String numeroUser = request.getParameter("champ["+i+"][0]");
+    			
+    			if (numeroUser != null)
+    			{
+    				// Récupération des valeurs des inputs
+    				String nomUser = request.getParameter("champ["+i+"][1]");
+    				String prenomUser = request.getParameter("champ["+i+"][2]");
+    				String mailUser = request.getParameter("champ["+i+"][3]");
+    				String nomPromotionUser = request.getParameter("champ["+i+"][4]");
+    				String nomGroupeUser = request.getParameter("champ["+i+"][5]");
+    				String nomSousGroupeUser = request.getParameter("champ["+i+"][6]");
+    				
+    				// ____________________________________
+    				// Gestion de l'élève en question
+    				List<User> usersList = em.createQuery("SELECT u FROM User u WHERE nom="+nomUser+" AND prenom="+prenomUser+" AND numero="+numeroUser, User.class).getResultList();
+    				User eleve = null;
+    				
+    				if(usersList.size() != 0)
+    				{
+    					// Extraction de l'élève
+    					eleve = (User) ObjectDBUtilServlet.extractOnlyOneObjectManagingExceptions(usersList);
+    				}
+    				else
+    				{
+    					// Création et enregistrement dans la BDD de l'eleve
+    					eleve = new User(nomUser, prenomUser, numeroUser, mailUser, false);
+    					em.getTransaction().begin();	
+    					em.persist(eleve);					
+    					em.getTransaction().commit();	// Attention !!! les id's ne sont générées qu'après le commit de l'instance persistante associée !
+    				}
+    				
+    				// ____________________________________
+    				// Recherche de la promo dans la BDD
+    				List<Promotion> promotionList = em.createQuery("SELECT p FROM Promotion p WHERE anneeObtensionDiplome="+Long.parseLong(nomPromotionUser), Promotion.class).getResultList();
+    				Promotion promotion = null;
+    				
+    				if (promotionList.size() != 0)
+    				{
+    					promotion = (Promotion) ObjectDBUtilServlet.extractOnlyOneObjectManagingExceptions(promotionList);
+    				}
+    				else
+    				{
+    					//Création et enregistrement dans la BDD de la promotion
+    					promotion = new Promotion(Long.parseLong(nomPromotionUser));
+    					
+    					em.getTransaction().begin();	
+    					em.persist(promotion);					
+    					em.getTransaction().commit(); // Attention !!! les id's ne sont générées qu'après le commit de l'instance persistante associée !
+    				}
+    				
+    				eleve.setPromotionID(promotion.getID());
+    				promotion.addEleveID(eleve.getID());
+    				
+    				em.getTransaction().begin();	
+    				em.persist(eleve);
+    				em.persist(promotion);
+    				em.getTransaction().commit();
+    				
+    				// ____________________________________
+    				// Recherche du groupe dans la BDD
+    				List<Groupe> groupesList = em.createQuery("SELECT g FROM Groupe g WHERE nom="+nomGroupeUser+" AND promotionID="+promotion.getID(), Groupe.class).getResultList();
+    				Groupe groupe = null;
+    				
+    				if(groupesList.size() != 0)
+    				{
+    					groupe = (Groupe) ObjectDBUtilServlet.extractOnlyOneObjectManagingExceptions(groupesList);
+    				}
+    				else
+    				{
+    					//Création et enregistrement dans la BDD du groupe
+    					groupe = new Groupe(nomGroupeUser);
+    					
+    					em.getTransaction().begin();	
+    					em.persist(groupe);					
+    					em.getTransaction().commit(); // Attention !!! les id's ne sont générées qu'après le commit de l'instance persistante associée !
+    				}
+    				
+    				eleve.setGroupeID(groupe.getID());
+    				promotion.addGroupeID(groupe.getID());
+    				groupe.addEleveID(eleve.getID());
+    				
+    				em.getTransaction().begin();
+    				em.persist(eleve);
+    				em.persist(promotion);
+    				em.persist(groupe);
+    				em.getTransaction().commit();
+    				
+    				// ____________________________________
+    				// Recherche du sous-groupe dans la BDD
+    				List<SousGroupe> sousGroupesList = em.createQuery("SELECT g FROM SousGroupe g WHERE nom="+nomSousGroupeUser+" AND groupeID="+groupe.getID(), SousGroupe.class).getResultList();
+    				SousGroupe sousGroupe = null;
+    				
+    				if(sousGroupesList.size() != 0)
+    				{
+    					sousGroupesList = (List<SousGroupe>) ObjectDBUtilServlet.extractMultipleObjectsManagingExceptions(sousGroupesList);
+    					for(SousGroupe ssgrp : sousGroupesList)
+    					{
+    						List<Groupe> grpsLst = em.createQuery("SELECT g FROM Groupe g WHERE id="+ssgrp.getGroupeID(), Groupe.class).getResultList();
+    						Groupe grp = (Groupe) ObjectDBUtilServlet.extractOnlyOneObjectManagingExceptions(grpsLst);
+    						
+    						if(grp.getPromotionID() == promotion.getID())
+    						{
+    							sousGroupe = ssgrp;
+    							break;
+    						}
+    					}
+    				}
+    				else
+    				{
+    					//Création et enregistrement dans la BDD du sous groupe
+    					sousGroupe = new SousGroupe(nomSousGroupeUser);
+    					
+    					em.getTransaction().begin();
+    					em.persist(sousGroupe);
+    					em.getTransaction().commit(); // Attention !!! les id's ne sont générées qu'après le commit de l'instance persistante associée !
+    				}
+    				
+    				eleve.setSousGroupeEleveID(sousGroupe.getID());
+    				sousGroupe.addEleveID(eleve.getID());
+    				groupe.addSousGroupeID(sousGroupe.getID());
+    				sousGroupe.setGroupeID(groupe.getID());
+    				
+    				em.getTransaction().begin();
+    				em.persist(eleve);
+    				em.persist(sousGroupe);
+    				em.persist(groupe);
+    				em.getTransaction().commit();
+    				// ____________________________________
+    			}
+    		}
+
+    		} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		// Close the database connection:
+    		if (em.getTransaction().isActive())
+    			em.getTransaction().rollback();
+    		em.close();
+    	}
+    }
+}
 
